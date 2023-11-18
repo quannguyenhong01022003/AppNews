1f03d00deb8d71bb2573f089162d71783d4f14ba
quannguyenhong01022003 <quannguyenhong01022003@gmail.com>
2023-11-18 18:46:08 +0700

    New list screen

--- index.js
+++ index.js
@@ -6,5 +6,7 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import ScreenLogin from './src/ScreenLogin';
+import ItemlistNews from './src/ItemlistNews';
+import ListNews from './src/ListNews';
 
-AppRegistry.registerComponent(appName, () => ScreenLogin);
+AppRegistry.registerComponent(appName, () => ListNews);
---
+++ src/ItemlistNews.js
@@ -0,0 +1,49 @@
+import {Image, StyleSheet, Text, View, Dimensions} from 'react-native';
+import React from 'react';
+
+const ItemlistNews = (props) => {
+    const {data} = props
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.images}
+        source={{uri: data.image}}/>
+      <View style={styles.content}>
+        <Text style={styles.textItem}>{data.title}</Text>
+        <Text numberOfLines={3} style={styles.textContent}>{data.content}</Text>
+      </View>
+    </View>
+  );
+};
+
+export default ItemlistNews;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    marginTop: 15
+  },
+  images: {
+    width: 96,
+    height: 96,
+    borderRadius:5
+  },
+  textItem: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    lineHeight: 19.5,
+    fontFamily: 'Poppins',
+    color: 'red',
+  },
+  textContent: {
+    fontWeight: '400',
+    marginTop: 5,
+    fontSize: 14,
+    lineHeight: 20,
+    color: '#000000',
+  },
+  content: {
+    start: 15,
+    width: Dimensions.get('window').width - 96 - 15
+  },
+});
